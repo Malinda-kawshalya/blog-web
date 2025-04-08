ff188305872c549666a1cdf1d5133de30aa3a498
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-04-09 01:24:05 +0530

    update

--- includes/config.php
+++ includes/config.php
@@ -6,5 +6,5 @@ define('DB_PASS', '');
 define('DB_NAME', 'blog_db');
 
 // Site configuration
-define('SITE_URL', 'http://localhost/blog');
+define('SITE_URL', 'http://localhost/blog%20web/blog-web');
 define('SITE_TITLE', 'My Blog');
